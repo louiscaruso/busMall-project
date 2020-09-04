c9fdedee3e05f3b1e3411cb82d99765b67b7cfc9
Louis Caruso <carusolouis00@gmail.com>
2020-09-04 13:29:26 -0700

    local storage added

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 'use strict';
 //code taken from in class demo
+
 var section = document.getElementById('results');
 var imgElOne = document.getElementById('image-one');
 var imgElTwo = document.getElementById('image-two');
 var imgElThree = document.getElementById('image-three');
-
-var clicksAllowed = 25;
+var clicksAllowed = 5;
 var numberOfClicks = 0;
 var renderArray = [];
 var imgArray = [];
 
+//constuctor
 function Picture(name, src) {
   this.viewed = 0;
   this.clicked = 0;
@@ -18,28 +19,36 @@ function Picture(name, src) {
   imgArray.push(this);
 }
 
-new Picture('bag', './img/bag.jpg');
-new Picture('banana', './img/banana.jpg');
-new Picture('bathroom', './img/bathroom.jpg');
-new Picture('boots', './img/boots.jpg');
-new Picture('breakfast', './img/breakfast.jpg');
-new Picture('bubblegum', './img/bubblegum.jpg');
-new Picture('chair', './img/chair.jpg');
-new Picture('cthulu', './img/cthulhu.jpg');
-new Picture('dog-duck', './img/dog-duck.jpg');
-new Picture('dragon', './img/dragon.jpg');
-new Picture('pen', './img/pen.jpg');
-new Picture('pet-sweep', './img/pet-sweep.jpg');
-new Picture('scissors', './img/scissors.jpg');
-new Picture('shark', './img/shark.jpg');
-new Picture('sweep', './img/sweep.png');
-new Picture('tauntaun', './img/tauntaun.jpg');
-new Picture('unicorn', './img/unicorn.jpg');
-new Picture('usb', './img/usb.gif');
-new Picture('water-can', './img/water-can.jpg');
-new Picture('wine-glass', './img/wine-glass.jpg');
+//local storage
+var savedProduct = localStorage.getItem('savedProducts');
+if (savedProduct) {
+  imgArray = JSON.parse(savedProduct);
+} else {
 
+  new Picture('bag', './img/bag.jpg');
+  new Picture('banana', './img/banana.jpg');
+  new Picture('bathroom', './img/bathroom.jpg');
+  new Picture('boots', './img/boots.jpg');
+  new Picture('breakfast', './img/breakfast.jpg');
+  new Picture('bubblegum', './img/bubblegum.jpg');
+  new Picture('chair', './img/chair.jpg');
+  new Picture('cthulu', './img/cthulhu.jpg');
+  new Picture('dog-duck', './img/dog-duck.jpg');
+  new Picture('dragon', './img/dragon.jpg');
+  new Picture('pen', './img/pen.jpg');
+  new Picture('pet-sweep', './img/pet-sweep.jpg');
+  new Picture('scissors', './img/scissors.jpg');
+  new Picture('shark', './img/shark.jpg');
+  new Picture('sweep', './img/sweep.png');
+  new Picture('tauntaun', './img/tauntaun.jpg');
+  new Picture('unicorn', './img/unicorn.jpg');
+  new Picture('usb', './img/usb.gif');
+  new Picture('water-can', './img/water-can.jpg');
+  new Picture('wine-glass', './img/wine-glass.jpg');
 
+}
+
+//random picture function
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
 }
@@ -77,8 +86,8 @@ function renderImages() {
 }
 
 function eventHandler(e) {
-  console.log('e', e);
-  console.log(e.target.alt);
+  // console.log('e', e);
+  // console.log(e.target.alt);
   numberOfClicks++;
   for (var i = 0; i < imgArray.length; i++) {
     if (imgArray[i].name === e.target.alt) {
@@ -92,16 +101,23 @@ function eventHandler(e) {
     imgElOne.removeEventListener('click', eventHandler);
     imgElTwo.removeEventListener('click', eventHandler);
     imgElThree.removeEventListener('click', eventHandler);
+
     for (i = 0; i < imgArray.length; i++) {
       var imageClickedAmount = document.createElement('ul');
-      imageClickedAmount.textContent = `${imgArray[i].name}, clicked ${imgArray[i].clicked} times, viewed ${imgArray[i].viewed} times.`;
-      console.log(`${imgArray[i].name}, clicked ${imgArray[i].clicked} times, viewed ${imgArray[i].viewed} times.`);
+      // imageClickedAmount.textContent = `${imgArray[i].name}, clicked ${imgArray[i].clicked} times, viewed ${imgArray[i].viewed} times.`;
+      // console.log(`${imgArray[i].name}, clicked ${imgArray[i].clicked} times, viewed ${imgArray[i].viewed} times.`);
       section.appendChild(imageClickedAmount);
     }
+    var stringifiedProducts = JSON.stringify(imgArray);
+    localStorage.setItem('savedProducts', stringifiedProducts);
+    // localStorageResults
+    console.log(savedProduct[0]);
+
     renderCharts();
   }
 }
 
+//where we add event
 imgElOne.addEventListener('click', eventHandler);
 imgElTwo.addEventListener('click', eventHandler);
 imgElThree.addEventListener('click', eventHandler);
@@ -130,7 +146,7 @@ function renderCharts() {
         label: '# of Clicks',
         data: clicksArray,
         backgroundColor: 'rgba(211, 204, 204, 0.973)',
-        borderColor:'rgba(0, 49, 211, 0.973)',
+        borderColor: 'rgba(0, 49, 211, 0.973)',
         borderWidth: 1
       },
       {
@@ -152,3 +168,7 @@ function renderCharts() {
     }
   });
 }
+
+console.log(imgArray);
+// document.getElementById('results').innerHTML = imgArray;
+
